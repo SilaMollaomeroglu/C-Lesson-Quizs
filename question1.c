ecdb7f538dc2bbe87fa3d53d53fd1e8a520f3e1e
SilaMollaomeroglu <69357065+SilaMollaomeroglu@users.noreply.github.com>
2021-11-11 20:36:26 +0300

    Add files via upload

---
+++ question1.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+
+int main()
+{
+	int value ;
+	
+	printf("Enter some integers: \n");
+	scanf("%d",&value);
+
+	int smallest;
+	int array[value], i, j, elements;
+	
+	array[0] = value;
+	for(i=1; i<value; i++)
+	{
+		scanf("%d", &elements);
+		array[i] = elements;
+	}
+	
+	smallest = array[0];
+	for( i=1; i<value; i++)
+	{
+	
+		if(array[i] < smallest)
+		{
+			smallest = array[i];
+		}		
+    }
+    
+	printf("smallest: ");
+	printf("%d", smallest);
+
+
+	return 0;
+}
